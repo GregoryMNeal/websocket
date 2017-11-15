da3702716b5958da09782ca7b91f6c143d327a5e
Gregory Neal <gnealsr@gmail.com>
2017-11-15 10:52:33 -0600

    Completed Chat Tutorial

--- app.js
+++ app.js
@@ -4,27 +4,17 @@
 var express = require('express'); // used to make an Express app
 var app = express(); // make the app
 var http = require('http').Server(app); //Node.js has a built-in module called HTTP which allows Node.js to transfer data over the Hyper Text Transfer Protocol (HTTP)
-var io = require('socket.io')(http); // Socket.IO enables real-time bidirectional event-based communication
-
-// Application setup
-app.set('view engine', 'hbs'); // use handlebars for template rendering
-app.use(express.static('public')); // eetup express to serve the files in the public folder
-app.use('/socket-io',express.static('node_modules/socket.io-client/dist'));
+var io = require('socket.io')(http);
 
 // home page
 app.get('/', function (req, resp, next) {
-  response.render('chat.hbs');
+  resp.sendFile(__dirname + '/views/chat.html');
 });
 
-io.on('connection', function(client){
-  console.log('CONNECTED');
-  client.on('disconnect', function () {
-    console.log('EXITED');
+io.on('connection', function(socket) {
+  socket.on('chat message', function(msg) {
+    io.emit('chat message', msg);
   });
-});
-
-client.on('incoming', function(msg){
-  io.emit('chat-msg', msg);
 });
 
 // Listen for requests
